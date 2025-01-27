aa4b6033cfd808fa95b2a8ed9c873303ecda13bb
Ivan24-wq <iv8747583@gmail.com>
2025-01-27 19:37:21 +0300

    coed

---
+++ JAva/studing-java/src/com/studing/Math.java
@@ -0,0 +1,10 @@
+//Составьте программу, выводящую на экран квадраты чисел от 10 до 20 включительно
+package com.studing;
+
+public class Main{
+    public static void main(String[] args){
+        for(int i = 10; i < 21; i++){
+            System.out.println("Ответ: " + i * i);
+        }
+    }
+}
